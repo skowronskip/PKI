30962ae5a64375e2bad271ff6c35f8b91032618c
Paweł Skowroński <p.skowronski@sharehire.pl>
2019-03-13 15:39:31 +0100

    switch plus to times

--- src/main/java/pl/pki/App.java
+++ src/main/java/pl/pki/App.java
@@ -9,10 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Result 2+3="+policz(2,3));
+        System.out.println("Result 2*3="+policz(2,3));
     }
 
     public static int policz(int a, int b) {
-        return a+b;
+        return a*b;
     }
 }
