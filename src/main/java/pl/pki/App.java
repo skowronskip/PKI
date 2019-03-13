131c39ff7419c3ad0281c99c6d75fc7128821181
Paweł Skowroński <p.skowronski@sharehire.pl>
2019-03-13 15:38:25 +0100

    Initial commit

---
+++ src/main/java/pl/pki/App.java
@@ -0,0 +1,18 @@
+package pl.pki;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Result 2+3="+policz(2,3));
+    }
+
+    public static int policz(int a, int b) {
+        return a+b;
+    }
+}
